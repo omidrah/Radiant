4ee3a8eab466ea6d28a2b8ed8fa6eb961e6c46f0
omidrah <com.rhm@gmail.com>
2024-05-06 18:10:00 +0330

    update ui l

--- Frontend/src/app/home/tablist/tablist.component.ts
+++ Frontend/src/app/home/tablist/tablist.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 @Component({
   selector: 'app-tablist', 
   templateUrl: './tablist.component.html',
   styleUrl: './tablist.component.css'
 })
-export class TablistComponent {
+export class TablistComponent implements OnInit {
+  @ViewChild('staticTabs') tabset: TabsetComponent;
 
+  constructor(){
+    
+  }  
+  ngOnInit(): void {
+    this.tabset.tabs[2].active=true;
+  }
+  activeTabId:string='cmd';
+  changeTab($event) {
+    this.activeTabId = $event?.id;
+    console.log(this.activeTabId);
+ }
 }
 /**change record to unrecord button */
 // $('#recButton').addClass("notRec");
