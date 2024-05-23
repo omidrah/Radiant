a6ffec4533ddbd104992f7730e1a3580e922cef7
omidrah <com.rhm@gmail.com>
2024-05-23 20:31:55 +0330

    update backend.... for buffer use in nodejs

--- Backend/server.js
+++ Backend/server.js
@@ -1,43 +1,51 @@
+
+
 // Example using Express.js
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const fs = require('fs');
 const date = require('date-and-time');
+const { buffer_alloc, buffer_write } = require('./buffer');
+
 
 
 const server = express();
 
-// Middleware to parse JSON data
-server.use(express.json());
+// // Middleware to parse JSON data
+// server.use(express.json());
 // Enable CORS for all routes
 server.use(cors());
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: true }));
+//server.use(bodyParser.json());
+//server.use(bodyParser.urlencoded({ extended: true }));
 // Example defining a route in Express
 server.get('/', (req, res) => {
-    res.send('<h1>Hello, Express.js Server!</h1>');
+    res.send('<h1>Hello, Radiant Company!</h1>');
 });
 
 // Handle POST requests to save data
 server.post('/api/save-data', (req, res) => {
     //const { name, message } = req.body;
     //const content = `Name: ${name}\nMessage: ${message}\n`;
-    let content = JSON.stringify(req.body, null, 2); // The third argument (2) adds indentation for readability
-    // console.log(content)
-    const now = new Date();
-    const formattedDateTime = date.format(now, 'YYYY-MM-DD-HH-mm-ss');
-    const filePath = `./datalogs/${formattedDateTime}.txt`;
-      // Save data to a file (e.g., data.txt)
-    fs.appendFile(filePath, content, (err) => {
-      if (err) {
-        console.error('Error saving data:', err);
-        res.status(500).send('Error saving data');
-      } else {
-        console.log('Data saved successfully!');
-        res.status(200).send('Data saved successfully');
-      }
-    });
+    let content = JSON.stringify(req.body); 
+    buffer_alloc(content.length)    
+    buffer_write(content)
+    /**generate file name */
+    // const now = new Date();
+    // const formattedDateTime = date.format(now, 'YYYY-MM-DD-HH-mm-ss');
+    // const filePath = `./datalogs/${formattedDateTime}.txt`;
+    
+    // // Save data to a file (e.g., data.txt)
+    // fs.createWriteStream(filePath, str, (err) => {
+    //   if (err) {
+    //     console.error('Error saving data:', err);
+    //     res.status(500).send('Error saving data');
+    //   } else {
+    //     console.log('Data saved successfully!');
+    //     res.status(200).send('Data saved successfully');
+    //   }
+    // });
+
   });
 
 // Include route files
