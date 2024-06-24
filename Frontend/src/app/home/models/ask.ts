e3d755b543dcf9784a2c0ee9123e05642aceef38
omid rahimi <com.rhm@gmail.com>
2024-06-24 18:48:24 +0330

    remove extra comment

--- Frontend/src/app/home/models/ask.ts
+++ Frontend/src/app/home/models/ask.ts
@@ -1,26 +1,18 @@
 export class askmodel {
     header:string;
-    // cmd: cmd;
     testmode: string;
     datamode: string;
     couple:number;
-    //unit:string;
-    //pwr:number;
     att:number
     /**freq uniq for all m tab */
     mfreq:number
-    rsvd1:number; //reserv1
+    rsvd1:number;
     /******** m1tab*/
     m1downdata: string;
     m1xm: number;
     m1ym:number;
     m1zm:number;
     m1status:number;
-    // m1selstatus:number
-    // m1counter:number
-    // m1common:number
-    // m1ontime:number
-    // m1linkled:number
     m1adm:string
     rsvd2:number;
     /******** */
@@ -29,11 +21,6 @@ export class askmodel {
     m2ym:number;
     m2zm:number;
     m2status:number;
-    // m2selstatus:number
-    // m2counter:number
-    // m2common:number
-    // m2ontime:number
-    // m2linkled:number
     m2adm:string
     rsvd3:number;
     /******** */
@@ -42,11 +29,6 @@ export class askmodel {
     m3ym:number;
     m3zm:number;
     m3status:number;
-    // m3selstatus:number
-    // m3counter:number
-    // m3common:number
-    // m3ontime:number
-    // m3linkled:number
     m3adm:string
     rsvd4:number;
     /******** */
@@ -55,12 +37,7 @@ export class askmodel {
     m4ym:number;
     m4zm:number;
     m4status:number;
-    // m4selstatus:number
-    // m4counter:number
-    // m4common:number
-    // m4ontime:number
-    // m4linkled:number
-    m4adm:string
+    m4adm:string;
     rsvd5:number;
     /******** */
     m5downdata: string;
@@ -68,11 +45,6 @@ export class askmodel {
     m5ym:number;
     m5zm:number;
     m5status:number;
-    // m5selstatus:number
-    // m5counter:number
-    // m5common:number
-    // m5ontime:number
-    // m5linkled:number
     m5adm:string
     rsvd6:number;
     /******** */
@@ -81,16 +53,10 @@ export class askmodel {
     m6ym:number;
     m6zm:number;
     m6status:number;
-    // m6selstatus:number
-    // m6counter:number
-    // m6common:number
-    // m6ontime:number
-    // m6linkled:number
     m6adm:string
-    /**checksum and reserve7 */
+    /** */
     checksum:number;
     rsvd7:number;
     /** */
     footer:string;
-
     };
--- Frontend/src/app/home/services/shared-form.service.ts
+++ Frontend/src/app/home/services/shared-form.service.ts
@@ -14,9 +14,7 @@ export class SharedFormService implements  OnDestroy  {
     datamode: 'manual',
     testmode: 'txoff',
     couple: 0,
-    //unit: 'w',
     att: 0,
-    //pwr: 0,
     /**unique for all tab */
     mfreq: 0,
     rsvd1:0,
@@ -26,11 +24,6 @@ export class SharedFormService implements  OnDestroy  {
     m1ym: 0,
     m1zm: 0,
     m1status: 0,
-    // m1selstatus: 0,
-    // m1counter: 0,
-    // m1common: 0,
-    // m1ontime: 0,
-    // m1linkled: 0,
     m1adm: '001101',
     rsvd2:0,
     /**m2 */
@@ -39,11 +32,6 @@ export class SharedFormService implements  OnDestroy  {
     m2ym: 0,
     m2zm: 0,
     m2status: 0,
-    // m2selstatus: 0,
-    // m2counter: 0,
-    // m2common: 0,
-    // m2ontime: 0,
-    // m2linkled: 0,
     m2adm: '001101',
     rsvd3:0,
     /**m3 */
@@ -52,11 +40,6 @@ export class SharedFormService implements  OnDestroy  {
     m3ym: 0,
     m3zm: 0,
     m3status: 0,
-    // m3selstatus: 0,
-    // m3counter: 0,
-    // m3common: 0,
-    // m3ontime: 0,
-    // m3linkled: 0,
     m3adm: '001101',
     rsvd4:0,
     /**m4 */
@@ -65,11 +48,6 @@ export class SharedFormService implements  OnDestroy  {
     m4ym: 0,
     m4zm: 0,
     m4status: 0,
-    // m4selstatus: 0,
-    // m4counter: 0,
-    // m4common: 0,
-    // m4ontime: 0,
-    // m4linkled: 0,
     m4adm: '001101',
     rsvd5:0,
     /**m5 */
@@ -78,11 +56,6 @@ export class SharedFormService implements  OnDestroy  {
     m5ym: 0,
     m5zm: 0,
     m5status: 0,
-    // m5selstatus: 0,
-    // m5counter: 0,
-    // m5common: 0,
-    // m5ontime: 0,
-    // m5linkled: 0,
     m5adm: '001101',
     rsvd6:0,
     /**m6 */
@@ -91,17 +64,11 @@ export class SharedFormService implements  OnDestroy  {
     m6ym: 0,
     m6zm: 0,
     m6status: 0,
-    // m6selstatus: 0,
-    // m6counter: 0,
-    // m6common: 0,
-    // m6ontime: 0,
-    // m6linkled: 0,
     m6adm: '001101',
     checksum:0,
     rsvd7:0,
     footer:'END'
   });
-  //currentData = this.formData.asObservable();
 
   get currentData(): Observable<askmodel> { return this.formData$.asObservable(); }
   constructor(private http: HttpClient) { }
@@ -114,8 +81,6 @@ export class SharedFormService implements  OnDestroy  {
         cData.testmode = newData['testmode'];
         cData.datamode = newData['datamode'];
         cData.couple = newData['couple'];
-        // cData.unit = newData['unit'];
-        // cData.pwr = newData['pwr'];
         cData.att =this.calcPout(newData['att']);
         break;
       case 'm1tab':
@@ -124,11 +89,6 @@ export class SharedFormService implements  OnDestroy  {
         cData.m1ym = newData['m1ym']
         cData.m1zm = newData['m1zm']
         cData.m1status = newData['m1status']
-        // cData.m1common = newData['m1common']
-        // cData.m1selstatus = newData['m1selstatus']
-        // cData.m1counter = newData['m1counter']
-        // cData.m1ontime = newData['m1ontime']
-        // cData.m1linkled = newData['m1linkled']
         cData.m1adm = newData['m1adm']
         cData.mfreq = newData['mfreq']
         break;
@@ -138,11 +98,6 @@ export class SharedFormService implements  OnDestroy  {
         cData.m2ym = newData['m2ym']
         cData.m2zm = newData['m2zm']
         cData.m2status = newData['m2status']
-        // cData.m2common = newData['m2common']
-        // cData.m2selstatus = newData['m2selstatus']
-        // cData.m2counter = newData['m2counter']
-        // cData.m2ontime = newData['m2ontime']
-        // cData.m2linkled = newData['m2linkled']
         cData.m2adm = newData['m2adm']
         cData.mfreq = newData['mfreq']
 
@@ -153,11 +108,6 @@ export class SharedFormService implements  OnDestroy  {
         cData.m3ym = newData['m3ym']
         cData.m3zm = newData['m3zm']
         cData.m3status = newData['m3status']
-        // cData.m3common = newData['m3common']
-        // cData.m3selstatus = newData['m3selstatus']
-        // cData.m3counter = newData['m3counter']
-        // cData.m3ontime = newData['m3ontime']
-        // cData.m3linkled = newData['m3linkled']
         cData.m3adm = newData['m3adm']
         cData.mfreq = newData['mfreq']
 
@@ -168,11 +118,6 @@ export class SharedFormService implements  OnDestroy  {
         cData.m4ym = newData['m4ym']
         cData.m4zm = newData['m4zm']
         cData.m4status = newData['m4status']
-        // cData.m4common = newData['m4common']
-        // cData.m4selstatus = newData['m4selstatus']
-        // cData.m4counter = newData['m4counter']
-        // cData.m4ontime = newData['m4ontime']
-        // cData.m4linkled = newData['m4linkled']
         cData.m4adm = newData['m4adm']
         cData.mfreq = newData['mfreq']
         break;
@@ -182,11 +127,6 @@ export class SharedFormService implements  OnDestroy  {
         cData.m5ym = newData['m5ym']
         cData.m5zm = newData['m5zm']
         cData.m5status = newData['m5status']
-        // cData.m5common = newData['m5common']
-        // cData.m5selstatus = newData['m5selstatus']
-        // cData.m5counter = newData['m5counter']
-        // cData.m5ontime = newData['m5ontime']
-        // cData.m5linkled = newData['m5linkled']
         cData.m5adm = newData['m5adm']
         cData.mfreq = newData['mfreq']
         break;
@@ -196,11 +136,6 @@ export class SharedFormService implements  OnDestroy  {
         cData.m6ym = newData['m6ym']
         cData.m6zm = newData['m6zm']
         cData.m6status = newData['m6status']
-        // cData.m6common = newData['m6common']
-        // cData.m6selstatus = newData['m6selstatus']
-        // cData.m6counter = newData['m6counter']
-        // cData.m6ontime = newData['m6ontime']
-        // cData.m6linkled = newData['m6linkled']
         cData.m6adm = newData['m6adm']
         cData.mfreq = newData['mfreq']
         break;
